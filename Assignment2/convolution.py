cba27629f3a5f10c66261df585569b4598f6972d
jullan-m <jmlindi@stud.ntnu.no>
2020-02-04 15:39:28 +0100

    Finished assignment

--- Assignment2/convolution.py
+++ Assignment2/convolution.py
@@ -8,68 +8,65 @@ from matplotlib import pyplot as plt
 from matplotlib import rc
 
 # Latex font rendering
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-## for Palatino and other serif fonts use:
-#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
-t_ = np.linspace(-10, 10, 501)
+def plot_signal(t, sig, range, name):
+    plt.figure()
+    plt.title(name)
+    plt.plot(t , sig)
+    plt.xlim(left=range[0], right=range[1])
+    plt.xlabel(r"Time, $t$ / ms")
+    plt.ylabel(r"Amplitude, $x(t)$", fontsize=16)
+    plt.grid()
 
+t = np.linspace(-70,70, 1401)
 
-plt.figure()
-plt.title("Rectangular waveform")
-plt.plot(t_ , np.heaviside(t_ + 5, 1) - np.heaviside(t_ - 5, 1))
-plt.xlabel(r"Time, $t$ / ms")
-plt.ylabel(r"Amplitude, $x(t)$", fontsize=16)
-plt.grid()
-#plt.savefig("waveform_rect.pdf")
-plt.show()
+name1 = "Rectangular waveform"
+sig1 = np.heaviside(t + 5, 1) - np.heaviside(t - 5, 1)
 
-sawt_sig = 0.5*(signal.sawtooth(4/15*np.pi*t_, 0) + 1)
-sawt_pwm = np.heaviside(t_, 1) - np.heaviside(t_ - 7.5, 1)
+name2 = "Triangular waveform"
+sawt_sig = 0.5*(signal.sawtooth(4/15*np.pi*t, 0) + 1)
+sawt_pwm = np.heaviside(t, 1) - np.heaviside(t - 7.5, 1)
+sig2 = sawt_pwm * sawt_sig
 
-plt.figure()
-plt.title("Triangular waveform")
-plt.plot(t_ , sawt_pwm * sawt_sig)
-plt.xlabel(r"Time, $t$ / ms")
-plt.ylabel(r"Amplitude, $x(t)$", fontsize=16)
-plt.grid()
-#plt.savefig("waveform_triang.pdf")
-plt.show()
-
+name3 = "Gaussian waveform"
 def gaussian(x, amp, width):
     sigma = np.sqrt(0.5*(width/2)**2/(1+ np.log(amp)))
     return amp*np.exp(-0.5*(x/sigma)**2)
 
-t_gauss = np.linspace(-20,20, 501)
-plt.figure()
-plt.title("Gaussian waveform")
-plt.axhline(1/np.e, c="k", label=r"$y=1/e$")
-plt.plot(t_gauss, gaussian(t_gauss, 1, 10), label=r"Gaussian pulse")
-plt.ylim(bottom=0)
-plt.xlabel(r"Time, $t$ / ms")
-plt.ylabel(r"Amplitude, $x(t)$", fontsize=16)
-plt.legend()
-plt.grid()
-#plt.savefig("waveform_gauss.pdf")
+sig3 = gaussian(t, 1, 10)
+
+name4 = "Sequence of 11 rectangular pulses"
+sig_k = 0.2
+rect_pwm = np.heaviside(t + 54, 1) - np.heaviside(t - 55, 1)
+rect_sig = 0.5*(signal.square(sig_k*np.pi*(t+1.5), duty=3/10)+1)
+sig4 = rect_pwm * rect_sig
+
+
+sig1_sig1 = np.convolve(sig1, sig1) / 10000
+sig1_sig2 = np.convolve(sig1, sig2) / 10000
+sig3_sig4 = np.convolve(sig3, sig4) / 10000
+sig2_sig4 = np.convolve(sig2, sig4) / 10000
+
+t_ = np.linspace(-140,140, 2801)
+
+plot_signal(t_, sig1_sig1, [-12,12], "Signal 1 convolved with signal 1")
+plt.ylabel(r"Convolution area", fontsize=16)
+plt.savefig("sig1_sig1.pdf")
 plt.show()
 
+plot_signal(t_, sig1_sig2, [-8,15], "Signal 1 convolved with signal 2")
+plt.ylabel(r"Convolution area", fontsize=16)
+plt.savefig("sig1_sig2.pdf")
+plt.show()
 
-t = np.linspace(-60,60, 501)
-sig_k = 0.2
+plot_signal(t_, sig3_sig4, [-70,70], "Signal 3 convolved with signal 4")
+plt.ylabel(r"Convolution area", fontsize=16)
+plt.savefig("sig3_sig4.pdf")
+plt.show()
 
-
-pwm = np.heaviside(t + 54, 1) - np.heaviside(t - 55, 1)
-sig = 0.5*(signal.square(sig_k*np.pi*(t+1.5), duty=3/10)+1)
-
-plt.figure()
-plt.title(r"Sequence of 11 rectangular pulses")
-#plt.plot(t,  0.5*(signal.square(0.5*np.pi*t)+1), c="g")
-plt.plot(t,  pwm*sig, c="b", linewidth = 0.75)
-#plt.plot(t,  pwm, c="r", linewidth = 0.75, linestyle='-.')
-plt.xlim(left=t.min(), right=t.max())
-plt.xlabel(r"Time, $t$ / ms")
-plt.ylabel(r"Amplitude, $x(t)$", fontsize=16)
-plt.grid()
-#plt.savefig("waveform_11rect.pdf")
+plot_signal(t_, sig2_sig4, [-60,60], "Signal 2 convolved with signal 4")
+plt.ylabel(r"Convolution area", fontsize=16)
+plt.savefig("sig2_sig4.pdf")
 plt.show()
--- Assignment2/time_transform.py
+++ Assignment2/time_transform.py
@@ -3,18 +3,14 @@ __author__ = 'Jullan'
 #Made by Jullan
 
 import numpy as np
-from scipy import signal
 from matplotlib import pyplot as plt
 from matplotlib import rc
-'''
-# Latex font rendering
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-## for Palatino and other serif fonts use:
-#rc('font',**{'family':'serif','serif':['Palatino']})
-rc('text', usetex=True)
-'''
 
-t = np.linspace(-3,3, 501)
+# Latex font rendering
+rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+
+t = np.linspace(-5,15, 1001)
 y = 1 - (t+1) * np.heaviside(t + 1, 0.5) + (3*t-3) * np.heaviside(t - 1, 0.5) \
     -(2*t-4) * np.heaviside(t - 2, 0.5)
 
@@ -22,27 +18,23 @@ fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
 
 ax1.plot(t, y)
 ax1.grid(True)
-ax1.set_ylabel(r'$y(t)$')
-ax1.set_title(r'$y(t) = x(t)$')
-#ax1.margins(0, 0.1)
+ax1.set_xlim(-5,15)
+ax1.set_ylabel(r'$y_1(t)$')
+ax1.set_title(r'$y_1(t) = x(t)$')
 
 ax2.plot(4*t + 3, y)
 ax2.grid(True)
-ax2.set_ylabel(r'$y(t)$')
-ax2.set_title(r'$y(t) = x(4t+3)$')
-#ax2.margins(0, 0.1)
+ax2.set_xlim(-5,15)
+ax2.set_ylabel(r'$y_2(t)$')
+ax2.set_title(r'$y_2(t) = x(4t+3)$')
 
 ax3.plot(-3*t+2, y)
 ax3.grid(True)
-ax3.set_ylabel(r'$y(t)$')
-ax3.set_title(r'$y(t) = x(-3t+2)$')
-#ax3.margins(0, 0.1)
-
+ax3.set_xlim(-5,15)
+ax3.set_ylabel(r'$y_3(t)$')
+ax3.set_title(r'$y_3(t) = x(-3t+2)$')
 
 plt.xlabel(r"$t$", fontsize=16)
-
-
 fig.tight_layout()
-
-#plt.savefig("waveform_rect.pdf")
+plt.savefig("time_transform.pdf")
 fig.show()
