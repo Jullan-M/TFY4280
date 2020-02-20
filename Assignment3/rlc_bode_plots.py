349205e8b41943e214c5b91009d4c51242612c96
jullan-m <jmlindi@stud.ntnu.no>
2020-02-20 11:58:25 +0100

    Final hand-in candidate 3

--- Assignment3/rlc_bode_plots.py
+++ Assignment3/rlc_bode_plots.py
@@ -11,18 +11,19 @@ L = 100.0E-6
 C = 5.0E-9
 RLC = R*L*C
 
-def amp(f):
-    return np.real(R / ( (R - RLC*(2 * np.pi * f)**2) + (2 * np.pi * f * L)**2 ) * \
-           (R - RLC * (2 * np.pi * f)**2 - 2j * np.pi * f * L))
+def amp(w):
+    return np.real(R / ( (R - RLC * w**2) + (w * L)**2 ) * \
+           (R - RLC * w**2 - 1j * w * L))
 
-def theta(f):
-    return np.arctan(2*np.pi*f * L / ((2*np.pi*f)**2 * RLC - R))
+def theta(w):
+    return np.arctan(w * L / (w**2 * RLC - R))
 
 f = np.logspace(3, 6, 1001)
 
+
 plt.figure()
 plt.title("Amplitude")
-plt.loglog(f, amp(f))
+plt.loglog(f, amp(2*np.pi*f))
 plt.xlabel(r"Frequency, $f$ / ms")
 plt.ylabel(r"Amplitude, $u_2/u_1$", fontsize=16)
 plt.grid()
@@ -31,7 +32,7 @@ plt.show()
 
 plt.figure()
 plt.title("Phase response")
-plt.semilogx(f, np.rad2deg(theta(f)))
+plt.semilogx(f, np.rad2deg(theta(2*np.pi*f)))
 plt.xlabel(r"Frequency, $f$ / ms")
 plt.ylabel(r"Phase angle, $\theta(f)$", fontsize=16)
 plt.grid()
