7b39298e6deab77f93424f2b6f4f6090a17a588c
jullan-m <jmlindi@stud.ntnu.no>
2020-02-27 22:37:41 +0100

    Cleaning up code for car audio analysis

--- Assignment5/car_audio_analysis.py
+++ Assignment5/car_audio_analysis.py
@@ -34,7 +34,7 @@ if (__name__ == '__main__'):
         cs2.append(Signal(car.fs, car[int(intrvls2[i][0]*car.fs):int(intrvls2[i][1]*car.fs)]))
         cs2[i].name = str(intrvls2[i][0]) + '-' + str(intrvls2[i][1])
 
-    f_l2, f_r2 = 1125, 1500
+    f_l2, f_r2 = 1135, 1500
     for c in cs2:
         c.plot_magnitude_spec(f_range=[f_l2,f_r2], amp=0.002)
         print('Max amp @', c.argmax_freq_within(f_l2, f_r2))
