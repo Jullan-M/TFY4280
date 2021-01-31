4ce86a2fd4d795025437366bd9c5519056b192c0
Jullan-M <jmihkkal_lindi@hotmail.com>
2021-01-31 10:10:18 +0100

    update 2021

--- Assignment3/rlc_par_bode_plots.py
+++ Assignment3/rlc_par_bode_plots.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 from matplotlib import pyplot as plt
 from matplotlib import rc
 
@@ -7,33 +8,29 @@ rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
 R = 1000.0
-L = 100.0E-3
-C = 5.0E-9
-RLC = R*L*C
+L = 100.0e-3
+C = 5.0e-9
 
-def tranfer_func(w):
-    return 1 - R / (R - w**2 * RLC + 1j * w*L)
+lti_sys = signal.lti([R*L*C, L, 0], [R*L*C, L, R])
 
-def theta(w):
-    H = tranfer_func(w)
-    return np.arctan(np.imag(H)/np.real(H))
-
-f = np.logspace(2, 7, 1001)
+w, magn, phase = lti_sys.bode(n=1000)
+f = w/(2*np.pi)
 
 plt.figure()
+plt.minorticks_on()
 plt.title("Frequency response amplitude")
-plt.loglog(f, np.abs(tranfer_func(f)))
-plt.xlabel(r"Frequency, $f$ / Hz")
-plt.ylabel(r"Amplitude, $|H(f)|$", fontsize=16)
+plt.semilogx(f, magn)
+plt.xlabel(r"Frequency, $f$ [Hz]")
+plt.ylabel(r"Magnitude response, $|H(f)|$ [dB]", fontsize=16)
 plt.grid()
 plt.savefig("amp_response.pdf")
 plt.show()
 
 plt.figure()
 plt.title("Frequency response phase")
-plt.semilogx(f, np.rad2deg(theta(f)))
-plt.xlabel(r"Frequency, $f$ / Hz")
-plt.ylabel(r"Phase angle, $\theta(f)$", fontsize=16)
+plt.semilogx(f, phase)
+plt.xlabel(r"Frequency, $f$ [Hz]")
+plt.ylabel(r"Phase angle, $\theta(f)$ [deg]", fontsize=16)
 plt.grid()
 plt.savefig("phase_response.pdf")
 plt.show()
