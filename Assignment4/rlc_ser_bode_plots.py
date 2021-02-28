6564ebdac494dbe2b36e5835d8950c310c753328
Jullan-M <jmihkkal_lindi@hotmail.com>
2021-02-28 18:37:09 +0100

    Update 2021

--- Assignment4/rlc_ser_bode_plots.py
+++ Assignment4/rlc_ser_bode_plots.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 from matplotlib import pyplot as plt
 from matplotlib import rc
 
@@ -7,70 +8,56 @@ rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
 R = 1000.0
-
 L1 = 0.5
-C1 = 500E-12
-wN1 = 1 / np.sqrt(L1 * C1)
-z1 = R/2 * np.sqrt(C1/L1)
+C1 = 500e-12
 
-L2 = 0.01125
-C2 = 22.51E-9
+L2 = 0.1/np.sqrt(2)
+C2 = np.sqrt(2)*1e-7
 
+rlc_lti = signal.lti([0, R*C1, 0], [L1*C1, R*C1, 1])
+bw_lti = signal.lti([0,0,1], [L2*C2, R*C2, 1])
 
-def rlc_H(w):
-    return (4 * (z1/wN1)**2 * w**2
-            + 1j * 2 * (z1/wN1) * (w - w**3 / wN1**2)) / \
-           (1 + (w/wN1)**4 + (4*(z1/wN1)**2 - 2/wN1**2)*w**2)
-
-def bw_H(w):
-    return 1 / (1 - L2*C2*w**2 + 1j * R*C2*w)
-
-def theta(w, H):
-    H = H(w)
-    return np.arctan(np.imag(H), np.real(H))
-
-f1 = np.logspace(3, 5, 1001)
-magn_resp1 = np.abs(rlc_H(2*np.pi*f1))
+w1 = 2*np.pi*np.logspace(3,5,1001)
+w1, magn_rlc, phase_rlc = rlc_lti.bode(w1)
+w2 = np.logspace(2,5,1001)
+w2, magn_bw, phase_bw = bw_lti.bode(w2)
 
 plt.figure()
 plt.title("Frequency response amplitude")
-plt.loglog(f1, magn_resp1)
-plt.xlabel(r"Frequency, $f$ / Hz")
-plt.ylabel(r"Amplitude, $|H(f)|$", fontsize=16)
+plt.semilogx(w1/(2*np.pi), magn_rlc)
+plt.xlabel(r"Frequency, $f$ [Hz]")
+plt.ylabel(r"Amplitude, $|H(f)|$ [dB]", fontsize=16)
 plt.grid()
 plt.savefig("rlc_amp.pdf")
 plt.show()
 
 plt.figure()
 plt.title("Frequency response phase")
-plt.semilogx(f1, np.rad2deg(theta(2*np.pi*f1, rlc_H)))
-plt.xlabel(r"Frequency, $f$ / Hz")
-plt.ylabel(r"Phase angle, $\theta(f)$", fontsize=16)
+plt.semilogx(w1/(2*np.pi), phase_rlc)
+plt.xlabel(r"Frequency, $f$ [Hz]")
+plt.ylabel(r"Phase angle, $\theta(f)$ [deg]", fontsize=16)
 plt.grid()
 plt.savefig("rlc_response.pdf")
 plt.show()
 
-f2 = np.logspace(2, 5, 1001)
-magn_resp2 = np.abs(bw_H(2*np.pi*f2))
-
 plt.figure()
 plt.title("Frequency response amplitude")
-plt.loglog(f2, magn_resp2)
-plt.plot([1E2, 1E4], [1/np.sqrt(2), 1/np.sqrt(2)], linestyle='--', c='r')
-plt.plot([1E4, 1E4], [magn_resp2.min(), 1/np.sqrt(2)], linestyle='--', c='r')
+plt.semilogx(w2, magn_bw)
+plt.plot([1E2, 1E4], [-3, -3], linestyle='--', c='r')
+plt.plot([1E4, 1E4], [magn_bw.min(), -3], linestyle='--', c='r')
 plt.xlim(left = 1E2)
-plt.ylim(bottom=magn_resp2.min())
-plt.xlabel(r"Frequency, $f$ / Hz")
-plt.ylabel(r"Amplitude, $|H(f)|$", fontsize=16)
+plt.ylim(bottom=magn_bw.min())
+plt.xlabel(r"Angular frequency, $\omega$ [rad/s]")
+plt.ylabel(r"Amplitude, $|H(\omega)|$ [dB]", fontsize=16)
 plt.grid()
 plt.savefig("butterworth_amp.pdf")
 plt.show()
 
 plt.figure()
 plt.title("Frequency response phase")
-plt.semilogx(f2, np.rad2deg(theta(2*np.pi*f2, bw_H)))
-plt.xlabel(r"Frequency, $f$ / Hz")
-plt.ylabel(r"Phase angle, $\theta(f)$", fontsize=16)
+plt.semilogx(w2, phase_bw)
+plt.xlabel(r"Angular frequency, $\omega$ [rad/s]")
+plt.ylabel(r"Phase angle, $\theta(\omega)$ [deg]", fontsize=16)
 plt.grid()
 plt.savefig("butterworth_phase.pdf")
 plt.show()
